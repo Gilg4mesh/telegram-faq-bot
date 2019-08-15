c115470696040fae998445818e9f3a8c83a4cfc6
gilg4mesh <freedomtomdestiny@gmail.com>
2019-08-16 03:53:50 +0800

    fix styles

--- index.js
+++ index.js
@@ -1,130 +1,146 @@
-const { TelegramBot, TelegramHandler } = require('bottender');
-const { createServer } = require('bottender/koa');
+const { TelegramBot, TelegramHandler } = require('bottender')
+const { createServer } = require('bottender/koa')
 const NodeCache = require('node-cache')
-const util = require('./util');
-const FAQ = require('./FAQ.json');
+const util = require('./util')
+const FAQ = require('./FAQ.json')
 
 const bot = new TelegramBot({
-  accessToken: process.env.ACCESS_TOKEN,
-});
+    accessToken: process.env.ACCESS_TOKEN,
+})
 
 const cache = new NodeCache({
-  stdTTL: 600
+    stdTTL: 600,
 })
 
 const handler = new TelegramHandler()
-  .onText('/start', async context => {
-    let keys = Object.keys(FAQ[0]['FAQ']);
-    let keyboard = [['/start'], ['/last_step']];
-    for (var i in keys)
-      keyboard.push([keys[i]]);
+    .onText('/start', async context => {
+        const keys = Object.keys(FAQ[0].FAQ)
+        const keyboard = [
+            ['/start'],
+            ['/last_step'],
+        ]
 
-    const options = {
-      reply_markup: {
-        'keyboard': keyboard
-      } 
-    };
+        keys.forEach(key => {
+          keyboard.push([key])
+        })
 
-    let reply = "FAQ:\n";
-    for (var i = 0; i < keys.length; i++)
-      reply = reply.concat("\n" + (i+1) + ". " + keys[i].toString());
+        const options = {
+            reply_markup: {
+                keyboard,
+            },
+        }
 
-    let history = cache.get(context._session.user.id);
-    if (history == undefined)
-      history = [];
+        let reply = 'FAQ:\n'
+        for (let i = 0; i < keys.length; i += 1) {
+            reply = reply.concat('\n' + (i + 1) + '. ' + keys[i].toString())
+        }
 
-    history.push('/start');
-    cache.set(context._session.user.id, history);
+        let history = cache.get(context._session.user.id)
+        if (history === undefined) {
+            history = []
+        }
 
-    await context.sendMessage(reply, options);
-  })
+        history.push('/start')
+        cache.set(context._session.user.id, history)
 
-  .onText('/last_step', async context => {
-    let keys = Object.keys(FAQ[0]['FAQ']);
-    let text = 'FAQ';
+        await context.sendMessage(reply, options)
+    })
 
-    let history = cache.get(context._session.user.id);
-    if (history != undefined) {
-      history.pop();
-      text = history[history.length - 1];
-      cache.set(context._session.user.id, history);
-    }
+    .onText('/last_step', async context => {
+        let keys = Object.keys(FAQ[0].FAQ)
+        let text = 'FAQ'
 
-    console.log("------" + cache.get(context._session.user.id));
+        const history = cache.get(context._session.user.id)
+        if (history !== undefined) {
+            history.pop()
+            text = history[history.length - 1]
+            cache.set(context._session.user.id, history)
+        }
+
+        const contents = util.findValue(FAQ, text)
+        const message = contents.toString()
+
+        if (Object.keys(contents).length > 0) {
+            keys = Object.keys(contents[0])
+        }
+
+        const keyboard = [
+            ['/start'],
+            ['/last_step'],
+        ]
+
+        keys.forEach(key => {
+          keyboard.push([key])
+        })
+
+        const options = {
+            reply_markup: {
+                keyboard,
+            },
+        }
+
+        if (keys[0] !== '0') {
+            let reply = 'FAQ:\n'
+            for (let i = 0; i < keys.length; i += 1) {
+                reply = reply.concat('\n' + (i + 1) + '. ' + keys[i].toString())
+            }
+
+            await context.sendMessage(reply, options)
+        } else await context.sendMessage(message)
+    })
+
+    .onEvent(async context => {
+        let keys = Object.keys(FAQ[0].FAQ)
+        let { text } = context.event
+        if ('0123456789'.indexOf(text) !== -1) {
+            text = ''
+        }
+        const contents = util.findValue(FAQ, text)
+        const message = contents.toString()
+
+        if (Object.keys(contents).length > 0) {
+            keys = Object.keys(contents[0])
+        }
+
+        const keyboard = [
+            ['/start'],
+            ['/last_step'],
+        ]
+
+        keys.forEach(key => {
+          keyboard.push([key])
+        })
+
+        const options = {
+            reply_markup: {
+                keyboard,
+            },
+        }
+
+        let history = cache.get(context._session.user.id)
+        if (history === undefined) {
+            history = []
+        }
+
+        history.push(text)
+        cache.set(context._session.user.id, history)
+
+        console.log(keys[0])
+        if (keys[0] !== '0') {
+            let reply = 'FAQ:\n'
+            for (let i = 0; i < keys.length; i += 1) {
+                reply = reply.concat('\n' + (i + 1) + '. ' + keys[i].toString())
+            }
+
+            await context.sendMessage(reply, options)
+        } else await context.sendMessage(message)
+    })
 
 
-    let contents = util.findValue(FAQ, text);
-    let message = contents.toString();
-    
-    if (Object.keys(contents).length > 0)
-      keys = Object.keys(contents[0]);
-    
-    let keyboard = [['/start'], ['/last_step']];
-    for (var i in keys)
-      keyboard.push([keys[i]]);
+bot.onEvent(handler)
 
-    const options = {
-      reply_markup: {
-        'keyboard': keyboard
-      }
-    };
-    
-    if (keys[0] != 0) {
-      let reply = "FAQ:\n";
-      for (var i = 0; i < keys.length; i++)
-        reply = reply.concat("\n" + (i+1) + ". " + keys[i].toString());
+const server = createServer(bot)
 
-      await context.sendMessage(reply, options);
-    }
-    else await context.sendMessage(message);
-  })
-
-  .onEvent(async context => {
-    let keys = Object.keys(FAQ[0]['FAQ']);
-    let { text } = context.event;
-    if ('0123456789'.indexOf(text) !== -1)
-      text = "";
-    let contents = util.findValue(FAQ, text);
-    let message = contents.toString();
-    
-    if (Object.keys(contents).length > 0)
-      keys = Object.keys(contents[0]);
-    
-    let keyboard = [['/start'], ['/last_step']];
-    for (var i in keys)
-      keyboard.push([keys[i]]);
-
-    const options = {
-      reply_markup: {
-        'keyboard': keyboard
-      }
-    };
-
-    let history = cache.get(context._session.user.id);
-    if (history == undefined)
-      history = [];
-
-    history.push(text);
-    cache.set(context._session.user.id, history);
-
-    console.log(cache.get(context._session.user.id));
-
-    if (keys[0] != 0) {
-      let reply = "FAQ:\n";
-      for (var i = 0; i < keys.length; i++)
-        reply = reply.concat("\n" + (i+1) + ". " + keys[i].toString());
-
-      await context.sendMessage(reply, options);
-    }
-    else await context.sendMessage(message);
-  });
-
-
-
-bot.onEvent(handler);
-
-const server = createServer(bot);
-
-server.listen(process.env.PORT,process.env.IP, () => {
-  console.log('Your code is running at heroku.');
-});
+server.listen(process.env.PORT, process.env.IP, () => {
+    console.log('Your code is running at heroku.')
+})
--- util.js
+++ util.js
@@ -1,28 +1,22 @@
-function findValue(obj, key) {
-    return findValuesHelper(obj, key, []);
-}
-
-function findValuesHelper(obj, key, list) {
-    if (!obj) return list;
+function findValue(obj, key, list = []) {
+    if (!obj) return list
     if (obj instanceof Array) {
-        for (var i in obj) {
-            list = list.concat(findValuesHelper(obj[i], key, []));
-        }
-        return list;
+        obj.forEach(content => {
+          list = list.concat(findValue(content, key, []))
+        })
+        return list
     }
-    if (obj[key]) list.push(obj[key]);
-
-    if ((typeof obj == "object") && (obj !== null)) {
-        var children = Object.keys(obj);
+    if (obj[key]) {
+        list.push(obj[key])
+    }
+    if ((typeof obj === 'object') && (obj !== null)) {
+        const children = Object.keys(obj)
         if (children.length > 0) {
-            for (i = 0; i < children.length; i++) {
-                list = list.concat(findValuesHelper(obj[children[i]], key, []));
+            for (let i = 0; i < children.length; i += 1) {
+                list = list.concat(findValue(obj[children[i]], key, []))
             }
         }
     }
-    return list;
+    return list
 }
-
-module.exports = {
-    findValue: findValue,
-};
+module.exports = { findValue: findValue}
